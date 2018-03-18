03b440882faacc423ea9b07471cca49f1699b639
Lenka Hedz <lenka-949@mail.ru>
2018-03-18 18:36:16 +0200

    load project

---
+++ HomeWork4/src/main/java/ua/training/controller/Controller.java
@@ -0,0 +1,47 @@
+package ua.training.controller;
+
+import ua.training.model.Model;
+import ua.training.model.Record;
+import ua.training.model.Row;
+import ua.training.view.View;
+
+import java.util.Scanner;
+
+public class Controller {
+
+    private Model model;
+    private View view;
+    private Scanner scanner;
+
+    public Controller(Model model, View view) {
+        super();
+        this.model = model;
+        this.view = view;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void processUser() {
+
+        Record record = model.getNotebook().getFirstRecord();
+
+        while (record.hasNextEmptyRow()) {
+            Row nextRow = record.getNextRow();
+            String value = inputStringValueWithScanner(nextRow.getRequest(), nextRow.getRegex());
+            record.writeNextRow(value);
+            record.setLastUpdate();
+        }
+
+        view.printFilledRecord(record);
+    }
+
+    public String inputStringValueWithScanner(String message, String regex) {
+        String res;
+        view.printStringInput(message);
+        while (!(scanner.hasNext() && (res = scanner.nextLine()).matches(regex) )) {
+            view.printWrongStringInput(message);
+        }
+        return res;
+    }
+
+}
+
